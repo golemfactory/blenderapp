2ec7446169239443e27c8c72b58a16946e977d9f
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2019-05-09 10:05:36 +0200

    Other approach to restarting, re-use the whole work-dir

--- tests/simulationbase.py
+++ tests/simulationbase.py
@@ -82,24 +82,22 @@ class SimulationBase(abc.ABC):
             self.loop.run_until_complete(golem_app.Verify(request))
         return reply.success
 
-    def _make_req_dirs(self, tmpdir):
-        req = tmpdir / f'req{random.random()}'
-        req_work = req / 'work'
-        req_resources = req / 'resources'
-        req_net_resources = req / 'network_resources'
-        req_results = req / 'results'
-        req_net_results = req / 'network_results'
-        for p in [req, req_work, req_resources, req_net_resources, req_results,
-                  req_net_results]:
-            p.mkdir()
-        return req_work, req_resources, req_net_resources, req_results, \
-            req_net_results
+    def _make_req_dirs(self, tmpdir, task_id, exist_ok=False):
+        req_work = tmpdir / task_id
+        req_resources = req_work / constants.RESOURCES_DIR
+        req_net_resources = req_work / constants.NETWORK_RESOURCES_DIR
+        req_results = req_work / constants.RESULTS_DIR
+        req_net_results = req_work / constants.NETWORK_RESULTS_DIR
+        result_tupple = (req_work, req_resources, req_net_resources,
+                         req_results, req_net_results)
+        for p in result_tupple:
+            p.mkdir(exist_ok=exist_ok)
+        return result_tupple
 
-    def _make_prov_dirs(self, tmpdir):
-        prov = tmpdir / f'prov{random.random()}'
-        prov_work = prov / 'work'
-        prov_net_resources = prov / 'network_resources'
-        for p in [prov, prov_work, prov_net_resources]:
+    def _make_prov_dirs(self, tmpdir, task_id):
+        prov_work = tmpdir / task_id
+        prov_net_resources = prov_work / constants.NETWORK_RESOURCES_DIR
+        for p in [prov_work, prov_net_resources]:
             p.mkdir()
         return prov_work, prov_net_resources
 
@@ -173,24 +171,13 @@ class SimulationBase(abc.ABC):
             time.sleep(3)
 
             task_id = 'test_task_id123'
-            req_task_work_dir = req_work_dir / task_id
-            req_task_work_dir.mkdir()
-            req_task_resources_dir = req_task_work_dir / constants.RESOURCES_DIR
-            req_task_resources_dir.mkdir()
-            req_task_net_resources_dir = \
-                req_task_work_dir / constants.NETWORK_RESOURCES_DIR
-            req_task_net_resources_dir.mkdir()
-            req_task_results_dir = req_task_work_dir / constants.RESULTS_DIR
-            req_task_results_dir.mkdir()
-            req_task_net_results_dir = \
-                req_task_work_dir / constants.NETWORK_RESULTS_DIR
-            req_task_net_results_dir.mkdir()
+            req_task_work_dir, req_task_resources_dir, \
+                req_task_net_resources_dir, req_task_results_dir, \
+                req_task_net_results_dir = \
+                self._make_req_dirs(req_work_dir, task_id)
 
-            prov_task_work_dir = prov_work_dir / task_id
-            prov_task_work_dir.mkdir()
-            prov_task_net_resources_dir = \
-                prov_task_work_dir / constants.NETWORK_RESOURCES_DIR
-            prov_task_net_resources_dir.mkdir()
+            prov_task_work_dir, prov_task_net_resources_dir = \
+                self._make_prov_dirs(prov_work_dir, task_id)
 
             self._put_cube_to_resources(req_task_resources_dir)
 
@@ -198,30 +185,15 @@ class SimulationBase(abc.ABC):
 
             for _ in range(task_params['subtasks_count']):
 
-                subtask_id, subtask_params = \
-                    self._get_next_subtask(requestor, task_id)
-                assert subtask_params['resources'] == [0]
-
-                self._copy_resources_from_requestor(
-                    req_task_net_resources_dir,
-                    prov_task_net_resources_dir,
-                    subtask_params,
-                )
-                prov_subtask_work_dir = prov_task_work_dir / subtask_id
-                prov_subtask_work_dir.mkdir()
-                self._compute(provider, task_id, subtask_id, subtask_params)
-                self._copy_result_from_provider(
-                    prov_subtask_work_dir,
-                    req_task_net_results_dir,
-                    subtask_id,
-                )
-
-                success = self._verify(
-                    requestor,
+                self._do_subtask(
                     task_id,
-                    subtask_id,
+                    requestor,
+                    provider,
+                    req_task_net_resources_dir,
+                    req_task_net_results_dir,
+                    prov_task_net_resources_dir,
+                    prov_task_work_dir
                 )
-                assert success
 
             for frame in expected_frames:
                 filename = f'result{frame:04d}.{task_params["format"]}'
@@ -230,6 +202,123 @@ class SimulationBase(abc.ABC):
         finally:
             self._close_server(requestor_server)
             self._close_server(provider_server)
+
+
+    def _simulate_restart(self, task_params: dict, tmpdir,
+                          expected_frames: list):
+
+        skipping_subtasks = 1
+
+        tmpdir = Path(tmpdir)
+        req_work_dir = tmpdir / 'requestor'
+        req_work_dir.mkdir()
+        prov_work_dir = tmpdir / 'provider'
+        prov_work_dir.mkdir()
+        print('tmpdir:', tmpdir)
+
+        requestor_port = 50005
+        requestor_server = self._spawn_server(req_work_dir, requestor_port)
+        provider_port = 50006
+        provider_server = self._spawn_server(prov_work_dir, provider_port)
+        try:
+            requestor = self._get_golem_app(requestor_port)
+            provider = self._get_golem_app(provider_port)
+            # Wait for the servers to be ready, I couldn't find a reliable
+            # way for that check
+            time.sleep(3)
+
+            task_id = 'test_task_id123'
+
+            req_task_work_dir, req_task_resources_dir, \
+                req_task_net_resources_dir, req_task_results_dir, \
+                req_task_net_results_dir = \
+                self._make_req_dirs(req_work_dir, task_id)
+
+            prov_task_work_dir, prov_task_net_resources_dir = \
+                self._make_prov_dirs(prov_work_dir, task_id)
+
+            self._put_cube_to_resources(req_task_resources_dir)
+
+            print('_create_task: ', task_id, ' params=', task_params)
+            self._create_task(requestor, task_id, task_params)
+
+            for _ in range(task_params['subtasks_count'] - skipping_subtasks):
+
+                self._do_subtask(
+                    task_id,
+                    requestor,
+                    provider,
+                    req_task_net_resources_dir,
+                    req_task_net_results_dir,
+                    prov_task_net_resources_dir,
+                    prov_task_work_dir
+                )
+
+            old_work_dir = req_task_work_dir
+
+            task_id = 'test_restarted_id123'
+            req_task_work_dir = req_work_dir / task_id
+            shutil.copytree(old_work_dir, req_task_work_dir)
+
+            req_task_work_dir, req_task_resources_dir, \
+                req_task_net_resources_dir, req_task_results_dir, \
+                req_task_net_results_dir = \
+                self._make_req_dirs(req_work_dir, task_id, exist_ok=True)
+
+            prov_task_work_dir, prov_task_net_resources_dir = \
+                self._make_prov_dirs(prov_work_dir, task_id)
+
+
+            print('_create_task: ', task_id, ' params=', task_params)
+            self._create_task(requestor, task_id, task_params)
+
+            for _ in range(skipping_subtasks):
+
+                self._do_subtask(
+                    task_id,
+                    requestor,
+                    provider,
+                    req_task_net_resources_dir,
+                    req_task_net_results_dir,
+                    prov_task_net_resources_dir,
+                    prov_task_work_dir
+                )
+
+            for frame in expected_frames:
+                filename = f'result{frame:04d}.{task_params["format"]}'
+                result_file = req_task_results_dir / filename
+                assert result_file.exists()
+        finally:
+            self._close_server(requestor_server)
+            self._close_server(provider_server)
+
+    def _do_subtask(self, task_id, requestor, provider,
+                    req_task_net_resources_dir, req_task_net_results_dir,
+                    prov_task_net_resources_dir, prov_task_work_dir):
+        subtask_id, subtask_params = self._get_next_subtask(requestor, task_id)
+        assert subtask_params['resources'] == [0]
+
+        self._copy_resources_from_requestor(
+            req_task_net_resources_dir,
+            prov_task_net_resources_dir,
+            subtask_params,
+        )
+        prov_subtask_work_dir = prov_task_work_dir / subtask_id
+        prov_subtask_work_dir.mkdir()
+        self._compute(provider, task_id, subtask_id, subtask_params)
+
+        self._copy_result_from_provider(
+            prov_subtask_work_dir,
+            req_task_net_results_dir,
+            subtask_id,
+        )
+
+        success = self._verify(
+            requestor,
+            task_id,
+            subtask_id,
+        )
+        assert success
 
     def test_one_subtasks_one_frame(self, tmpdir):
         self._simulate(self._get_cube_params(1, "1"), tmpdir, [1])
@@ -248,3 +337,6 @@ class SimulationBase(abc.ABC):
 
     def test_four_subtasks_two_frames(self, tmpdir):
         self._simulate(self._get_cube_params(4, "6-7"), tmpdir, [6, 7])
+
+    def test_restart(self, tmpdir):
+        self._simulate_restart(self._get_cube_params(2, "5;9"), tmpdir, [5, 9])
