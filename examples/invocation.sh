448376d5eac821375f2440f8a2d4fdd3466a82c8
Dariusz Rybi <jiivanq@gmail.com>
2020-04-24 09:59:59 +0200

    [review] examples directory

---
+++ examples/invocation.sh
@@ -0,0 +1,1 @@
+python scripts/task_api_tests/basic_integration.py task-from-app-def ../blenderapp/dist/golemfactory_blenderapp_0.8.0_d79c094407c070a67364ed65872d481d.json ../blenderapp/taskapicube.json --resources ./scripts/node_integration_tests/tasks/cube/cube.blend
